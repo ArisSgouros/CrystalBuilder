d45107b834cc8825a64109a6a26801046a52a4c8
ArisSgouros <arissgouros@gmail.com>
2024-08-01 16:30:43 +0300

    test: upd tests

--- example/graphene/run.sh
+++ example/graphene/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python ../../crystal_builder.py in.basis_graphene.dat "20,10,1" --file_pos="o.20_10.dat" --file_dump="o.20_10.lammpstrj" --file_xyz="o.20_10.xyz" --rc="0.0" --drc=0.1 > o.log
-python ../../crystal_builder.py in.basis_graphene.dat "30,4,1" --file_pos="o.30_4.dat" --file_dump="o.30_4.lammpstrj" --file_xyz="o.30_4.xyz" --rc="0.0" --drc=0.1 > o.log
-python ../../crystal_builder.py in.basis_graphene.dat "200,4,1" --file_pos="o.200_4.dat" --file_dump="o.200_4.lammpstrj" --file_xyz="o.200_4.xyz" --rc="0.0" --drc=0.1 > o.log
+python ../../crystal_builder.py in.basis_graphene.dat "20,10,1" --file_pos="o.20_10.dat" --file_dump="o.20_10.lammpstrj" --file_xyz="o.20_10.xyz" --rc="1.5" --drc=0.1 --angle=1 --dihed=1 > o.20_10.log
+python ../../crystal_builder.py in.basis_graphene.dat "30,4,1"  --file_pos="o.30_4.dat"  --file_dump="o.30_4.lammpstrj"  --file_xyz="o.30_4.xyz"  --rc="1.5" --drc=0.1 --angle=1 --dihed=1 > o.30_4.log
--- example/hexa_meso/run.sh
+++ example/hexa_meso/run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python ../../crystal_builder.py in.basis_hexa.dat "10,2,1" --file_pos="o.hexa.dat" --file_dump="o.hexa.lammpstrj" --file_xyz="o.hexa.xyz" --rc="1.0" --drc=0.1 > o.log
+python ../../crystal_builder.py \
+  in.basis_hexa.dat "10,2,1" \
+  --file_pos="o.hexa.dat" \
+  --file_dump="o.hexa.lammpstrj" \
+  --file_xyz="o.hexa.xyz" \
+  --rc="1.0" \
+  --drc=0.1 \
+  --angle=1 \
+  --dihed=1 # > o.log
