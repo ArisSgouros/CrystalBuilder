7ed88a8c58702553c4b5132af58baa8bcce4bf73
ArisSgouros <arissgouros@gmail.com>
2024-08-01 08:44:12 +0300

    test(hexa_meso): add bond

--- example/hexa_meso/run.sh
+++ example/hexa_meso/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ../../crystal_builder.py in.basis_hexa.dat "10,2,1" --file_pos="o.hexa.dat" --file_dump="o.hexa.lammpstrj" --file_xyz="o.hexa.xyz" --rc="0.0" --drc=0.1 > o.log
+python ../../crystal_builder.py in.basis_hexa.dat "10,2,1" --file_pos="o.hexa.dat" --file_dump="o.hexa.lammpstrj" --file_xyz="o.hexa.xyz" --rc="1.0" --drc=0.1 > o.log
