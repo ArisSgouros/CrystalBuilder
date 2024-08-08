1f25f6437dc1cb5892e1a443102228d49604f5f5
ArisSgouros <arissgouros@gmail.com>
2024-08-08 12:06:09 +0300

    test: upd hbn

--- example/hbn/run.sh
+++ example/hbn/run.sh
@@ -9,4 +9,18 @@ python ../../crystal_builder.py \
   --bond=1 \
   --angle=1 \
   --dihed=1 \
+  --cis_trans=0 \
    > o.log
+
+python ../../crystal_builder.py \
+  in.basis_hbn.dat "4,4,2" \
+  --file_pos="o.hbn_cis_trans.dat" \
+  --file_dump="o.hbn_cis_trans.lammpstrj" \
+  --file_xyz="o.hbn_cis_trans.xyz" \
+  --rc="1.44568507405082" \
+  --drc=0.1 \
+  --bond=1 \
+  --angle=1 \
+  --dihed=1 \
+  --cis_trans=1 \
+   > o.log_cis_trans
